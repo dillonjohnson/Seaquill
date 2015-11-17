cb41e466851fa0d3f1242c5d6ddcf139b912cfe9
derekfreed <derekf@uga.edu>
2015-11-17 16:41:53 -0500

    rails website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seaquill::Application.config.secret_key_base = '02363956a4b41d3cd077e5f90c4ad51f0d2b193bcda112322dab8d8cfe8c590edf17c02a5be1dabc5780ceb9da0b18f1422e096ac8a8b1e87da4665f9ea03f81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Seaquill::Application.config.session_store :cookie_store, key: '_seaquill_session'
