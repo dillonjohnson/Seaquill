3a236727fa8d87b85d0ef00947e02ef1805e1c1b
derekfreed <derekf@uga.edu>
2015-11-28 22:42:13 -0500

    product changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Product.create(productName: 'Stella Artois', pricePerUnit: '30.00', stock: '100', image: 'stella.jpg')
+#Product.create(productName: 'Bud Light', pricePerUnit: '25.00', stock: '100', image: 'budlight.jpg')
+Product.create(productName: 'Blue Moon', pricePerUnit: '28.00', stock: '100', image: 'bluemoon.jpg')
+Product.create(productName: 'Budweiser', pricePerUnit: '25.50', stock: '100', image: 'budweiser.jpg')
+Product.create(productName: 'Creature Comforts Tropicalia', pricePerUnit: '32.00', stock: '100', image: 'cctropicalia.jpg')
+Product.create(productName: 'Coors Light', pricePerUnit: '25.00', stock: '100', image: 'coors.jpg')
+Product.create(productName: 'Corona Extra', pricePerUnit: '26.50', stock: '100', image: 'corona.jpg')
+Product.create(productName: 'New Belgium Fat Tire', pricePerUnit: '28.00', stock: '100', image: 'fattire.jpg')
+Product.create(productName: 'Heineken', pricePerUnit: '28.00', stock: '100', image: 'heineken.jpg')
+Product.create(productName: 'Miller Lite', pricePerUnit: '25.00', stock: '100', image: 'millerlite.jpeg')
+Product.create(productName: 'Pabst Blue Ribbon', pricePerUnit: '25.00', stock: '100', image: 'pbr.jpg')
+Product.create(productName: 'Red Stripe', pricePerUnit: '25.00', stock: '100', image: 'redstripe.jpg')
+Product.create(productName: 'Samuel Adams Boston Lager', pricePerUnit: '27.80', stock: '100', image: 'samadams.jpg')
+Product.create(productName: 'Sierra Nevada Pale Ale', pricePerUnit: '31.00', stock: '100', image: 'sierranevada.jpg')
+Product.create(productName: 'Terrapin Hopsecutioner', pricePerUnit: '32.00', stock: '100', image: 'terrapinhopsecutioner.jpg')
+Product.create(productName: 'Terrapin Rye Pale Ale', pricePerUnit: '32.00', stock: '100', image: 'terrapinrye.jpg')
